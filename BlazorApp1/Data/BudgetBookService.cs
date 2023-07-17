9fa031cddcecfd008771e820c37ba13fd4ff83f2
Aekkasit Aisuwan <aekkasitrun@gmail.com>
2023-07-17 14:27:55 +0700

    clean code

---
+++ BlazorApp1/Data/BudgetBookService.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Data
+{
+    public class BudgetBookService
+    {
+       private static List<Entry> mockDb = new List<Entry>();
+        public async Task<bool> AddEntry(Entry newEntry)
+        {
+            mockDb.Add(newEntry);
+            return true;
+        }
+        public async Task<List<Entry>> GetAllEntries()
+        {
+            return mockDb;
+        }
+    }
+}
