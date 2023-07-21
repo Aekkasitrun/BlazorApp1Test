e1d59c87aa2204946b3f45f7b6e404052a1ff4e2
Aekkasit Aisuwan <aekkasitrun@gmail.com>
2023-07-21 13:21:38 +0700

    complete clear popup

--- BlazorApp1/Data/Entry.cs
+++ BlazorApp1/Data/Entry.cs
@@ -5,7 +5,7 @@ namespace BudgetBook.Data
     public class Entry
     {
         //public int Id { get; set; }
-        [Required (ErrorMessage ="กรุณาเพิ่มรายการ")] public string Description { get; set; }
+        [Required (ErrorMessage ="กรุณาเพิ่มรายการ")] public string? Description { get; set; }
         //public double Amount { get; set; }
         public bool IsChecked { get; set; }
     }
