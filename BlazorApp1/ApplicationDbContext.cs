b73a2bf16c74729a74a7d334af9b1fc2e9b697f1
Aekkasit Aisuwan <aekkasitrun@gmail.com>
2023-07-24 11:39:03 +0700

    complete create database

---
+++ BlazorApp1/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BudgetBook.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Entry> Entries { get; set; }
+
+    }
+}
